6a6ccf0d08f3894faa1cbce7c7ca1291df6827ad
Adrian Kaminski <adrian.kaminski@kodilla.com>
2022-03-04 23:09:44 +0100

    changes to uri

--- allocate/config.py
+++ allocate/config.py
@@ -1,11 +1,16 @@
 import os
 
+uri = os.environ.get("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+
 class Config:
     MAX_CONTENT_LENGTH = 8192 * 8192
     UPLOAD_EXTENSIONS = ['.jpg', '.png', '.pdf']
     SECRET_KEY = os.environ.get("SECRET_KEY")
     UPLOAD_PATH = 'uploads'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = uri
     LOGIN_DISABLED = False
     SECURITY_PASSWORD_SALT = os.environ.get("SECURITY_PASSWORD_SALT")
     MAIL_SERVER = 'smtp.googlemail.com'
